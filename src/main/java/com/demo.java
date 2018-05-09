2ec65488b901321a65c67494ed6012c4d89a951b
mengqingwei11 <39083161+mengqingwei11@users.noreply.github.com>
2018-05-09 17:55:31 +0800

    Update demo.java

--- src/main/java/com/demo.java
+++ src/main/java/com/demo.java
@@ -6,8 +6,7 @@ package com;
 public class demo {
     public void say(){
         System.out.println("s is a test");
-        System.out.println("two");
-        System.out.println("two");
+   
         System.out.println("two");
     }
 }
